74a18bddfd61c5f9e1854867d235d5a040905ecb
Huascar <huascarmc@hotmail.com>
2017-12-19 15:55:01 +0000

    server is listening

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const server = require('express');
+const parser = require('body-parser');
+const app = server();
+
+//Not clear what this three lines of code do ->
+app.use(parser.json());
+app.use(server.static('client/build'));
+app.use(parser.urlencoded({extended: true}));
+
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
