c08111576bad4a37aa99b995f0b972e8fe9e42be
Huascar <huascarmc@hotmail.com>
2017-12-19 16:11:01 +0000

    added build to package and installed mongodb

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const server = require('express');
 const parser = require('body-parser');
 const app = server();
 
+const MongoClient = require('mongodb').MongoClient;
+
 //Not clear what this three lines of code do ->
 app.use(parser.json());
-app.use(server.static('client/build'));
 app.use(parser.urlencoded({extended: true}));
 
-app.listen(3000, function(){
+MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+ if(err) {
+ console.log(err);
+ return;
+}
+
+const db = client.db("hammocks");
+
+console.log("connected to database");
+
+app.listen(3000, function(){9
   console.log("Listening on port 3000");
+ });
 });
