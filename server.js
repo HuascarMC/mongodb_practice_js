2f11ae5a4c68cb4af3ead95c8aca9db6cad7ea5f
Huascar <huascarmc@hotmail.com>
2017-12-19 16:47:02 +0000

    CRD methods added

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const server = require('express');
+const express = require('express');
 const parser = require('body-parser');
-const app = server();
+const server = express();
 
 const MongoClient = require('mongodb').MongoClient;
 
 //Not clear what this three lines of code do ->
-app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
+server.use(parser.json());
+server.use(parser.urlencoded({extended: true}));
 
 MongoClient.connect('mongodb://localhost:27017', function(err, client) {
  if(err) {
@@ -18,7 +18,51 @@ const db = client.db("hammocks");
 
 console.log("connected to database");
 
-app.listen(3000, function(){9
+server.get('/hammocks', function(req, res) {
+ db.collection('hammocks').find().toArray(function(err, result) {
+    if(err) {
+     console.log(err);
+     res.status(500);
+     res.send();
+     return;
+    }
+
+    console.log("Found");
+    res.status(201);
+    res.json(result);
+   });
+});
+
+server.post('/hammocks', function(req, res) {
+ db.collection('hammocks').insert( req.body, function(err, result) {
+   if(err) {
+    console.log(err);
+    res.status(500); // 500, something went wrong.
+    res.send(); // to whoever did the request.
+    return;
+   }
+
+   console.log("Saved to database");
+   res.status(201); // 201, all good.
+   res.json(result);
+ });
+});
+
+server.delete('/hammocks', function(req, res) {
+ db.collection('hammocks').remove(function(err, result) {
+  if(err) {
+   console.log(err);
+   res.status(500);
+   res.send();
+   return;
+  }
+
+  console.log("Deleted");
+  res.json(result);
+ });
+});
+
+server.listen(3000, function(){
   console.log("Listening on port 3000");
  });
 });
